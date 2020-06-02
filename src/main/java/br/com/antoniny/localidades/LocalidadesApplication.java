bd3e6d44f4b65e847f28aebaadde2bc43361872b
Antoniny <antoniny@gmail.com>
2020-06-02 10:07:39 -0300

    Feign - Inclusão - V2 Api
    Feign - Inclusão - V2 APi

--- src/main/java/br/com/antoniny/localidades/LocalidadesApplication.java
+++ src/main/java/br/com/antoniny/localidades/LocalidadesApplication.java
@@ -3,9 +3,11 @@ package br.com.antoniny.localidades;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
+@EnableFeignClients
 public class LocalidadesApplication {
 
     public static void main(String[] args) {
